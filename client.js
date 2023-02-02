0a505ba2022efeaecf2af3106b5b7c34fe64ade1
xjzh123 <37945610+xjzh123@users.noreply.github.com>
2023-02-02 20:48:10 +0800

    add missing length unit px

--- client.js
+++ client.js
@@ -141,7 +141,7 @@ var verifyNickname = function (nick) {
 //LaTeX weapon and too-many-quotes weapon defence
 function verifyMessage(args) {
 	// Shabby iOS Safari doesn't support zero-width assertion
-	if (/([^\s^_]+[\^_]{){8,}|(^|\n)(>[^>\n]*){5,}/.test(args.text) || /\$.*[[{]\d+(?:mm|pt|bp|dd|pc|sp|cm|cc|in|ex|em)[\]}].*\$/.test(args.text) || /\$\$[\s\S]*[[{]\d+(?:mm|pt|bp|dd|pc|sp|cm|cc|in|ex|em)[\]}][\s\S]*\$\$/.test(args.text)) {
+	if (/([^\s^_]+[\^_]{){8,}|(^|\n)(>[^>\n]*){5,}/.test(args.text) || /\$.*[[{]\d+(?:mm|pt|bp|dd|pc|sp|cm|cc|in|ex|em|px)[\]}].*\$/.test(args.text) || /\$\$[\s\S]*[[{]\d+(?:mm|pt|bp|dd|pc|sp|cm|cc|in|ex|em|px)[\]}][\s\S]*\$\$/.test(args.text)) {
 		return false;
 	} else {
 		return true;
