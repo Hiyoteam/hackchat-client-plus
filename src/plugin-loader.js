75b6d9588862e7f936d110cc6eccc68248a149a0
Maggie <96947247+MaggieLOL@users.noreply.github.com>
2023-04-01 17:35:34 +0800

    Update plugin-loader.js

--- src/plugin-loader.js
+++ src/plugin-loader.js
@@ -1,8 +1,10 @@
 // Load all allowed plugins in the localStorage
 plugins=JSON.parse(localstorageGet("plugins"))
+console.log("Loading plugins:",plugins)
 // add into the head
 plugins.forEach(element => {
     e=document.createElement("script")
     e.setAttribute("src",element)
     document.head.appendChild(e)
+    console.log("Loaded plugin: ",element)
 });
