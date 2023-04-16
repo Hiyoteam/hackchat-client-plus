2816a5b8b0dc8bddcb317a91fc35a129ad0a1746
Maggie <96947247+MaggieLOL@users.noreply.github.com>
2023-04-16 21:19:57 +0800

    Update plugin-loader.js

--- src/plugin-loader.js
+++ src/plugin-loader.js
@@ -1,10 +1,14 @@
-// Load all allowed plugins in the localStorage
-var plugins=JSON.parse(localStorageGet("plugins"))
-console.log("Loading plugins:",plugins)
-// add into the head
-plugins.forEach(element => {
-    let e=document.createElement("script")
-    e.setAttribute("src",element)
-    document.head.appendChild(e)
-    console.log("Loaded plugin: ",element)
-});
+try{
+    // Load all allowed plugins in the localStorage
+    var plugins=JSON.parse(localStorageGet("plugins"))
+    console.log("Loading plugins:",plugins)
+    // add into the head
+    plugins.forEach(element => {
+        let e=document.createElement("script")
+        e.setAttribute("src",element)
+        document.head.appendChild(e)
+        console.log("Loaded plugin: ",element)
+    });
+}catch(e){
+    console.warn("Error when loading plugins")
+}
