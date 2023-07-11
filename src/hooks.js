9e43ed2c118c09701ee2c51f57acc362513846c1
tanhanze <tanhanze@qq.com>
2023-07-11 11:42:04 +0800

    format&update

--- src/hooks.js
+++ src/hooks.js
@@ -1,19 +1,22 @@
-var hooks={}
+var hooks = {}
 // Format like: 
 // {"commandname":{"before":[func1,func2...],"in":[func1,func2,...],"after":[func1,func2,...]}}
-var hook={} // for storage the hook system
-hook.register = function(name, when, func) {
-    if (!hooks[name]){
+var hook = {} // for storage the hook system
+hook.register = function (when, name, func) {
+    if (!hooks[name]) {
         // Create
-        hooks[name]={"before":[],"in":[],"after":[]}
+        hooks[name] = { "before": [], "in": [], "after": [] }
     }
     // And the push the command
     hooks[name][when].push(func)
 }
-hook.run = function(name,when,args){
-    let funcs=hooks[name] ?? {"before":[],"in":[],"after":[]}
+hook.run = function (when, name, args) {
+    let funcs = hooks[name] ?? { "before": [], "in": [], "after": [] }
     funcs[when].forEach(element => {
         args = element(args)
+        if (args == false) {
+            return false //prevent this event run
+        }
     })
     return args
 }
