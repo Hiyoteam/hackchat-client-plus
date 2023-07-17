dba3938c779040fd575f04f2414eb35b7dead617
MaggieLOL <tanhanze@qq.com>
2023-07-17 10:56:28 +0800

    hotload-plugin

--- src/fancy.js
+++ src/fancy.js
@@ -86,6 +86,12 @@ let run = {
 		//save
 		localStorageSet("plugins",JSON.stringify(plugins))
 		pushMessage({nick:"*",text:"Added plugin."})
+		//load it NOW
+		let e = document.createElement("script")
+        e.setAttribute("src", plugin_address)
+        e.setAttribute("type","application/javascript");
+        document.getElementsByTagName('head')[0].appendChild(e); 
+        console.log("Loaded plugin: ", element)
 	},
 	listplugins(...args){
 		let plugins=localStorageGet("plugins")
