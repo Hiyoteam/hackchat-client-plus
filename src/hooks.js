83c76cc1aa8865e93f8aae77d607da3a92a93c0f
Yuka <admin@awaland.xyz>
2024-08-17 17:09:01 +0800

    feat: 减少函数对象的创建并简化条件判断（引入 ES6）

--- src/hooks.js
+++ src/hooks.js
@@ -1,25 +1,24 @@
-var hooks = {}
-// Format like:
-// {"commandname":{"before":[func1,func2...],"in":[func1,func2,...],"after":[func1,func2,...]}}
-var hook = {} // for storage the hook system
-hook.register = function (when, name, func) {
-    if (!hooks[name]) {
-        // Create
-        hooks[name] = { "before": [], "in": [], "after": [] }
-    }
-    // And the push the command
-    hooks[name][when].push(func)
-}
-hook.run = function (when, name, args) {
-    let funcs = hooks[name] ?? { "before": [], "in": [], "after": [] }
-    for (let element of funcs[when]) {
-        let result = element(args)
-        if (result === false) {
-            return false //prevent this event run
+var hooks = {};
+const defaultHook = { "before": [], "in": [], "after": [] };
+
+var hook = {
+    register: function (when, name, func) {
+        if (!hooks[name]) {
+            hooks[name] = { ...defaultHook };
         }
-        if (Array.isArray(result)) {
-            args = result
+        hooks[name][when].push(func);
+    },
+    run: function (when, name, args) {
+        let funcs = hooks[name]?.[when] ?? [];
+        for (let func of funcs) {
+            let result = func(args);
+            if (result === false) {
+                return false; // Prevent this event from running
+            }
+            if (Array.isArray(result)) {
+                args = result;
+            }
         }
+        return args;
     }
-    return args
-}
+};
